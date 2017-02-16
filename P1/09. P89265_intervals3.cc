3b5d936546f3f350a68b08142f2092326a47b2c2
julsgasull <juliagasulltw@gmail.com>
2017-02-16 11:40:54 +0100

    Update 09. P89265_intervals3.cc

--- P1/09. P89265_intervals3.cc
+++ P1/09. P89265_intervals3.cc
@@ -7,79 +7,19 @@ int main() {
 	int a1, b1, a2, b2;
 	cin >> a1 >> b1 >> a2 >> b2;
 	
-	if (a1 == a2 and b1 == b2) cout << "=";
-	else if (a2 < a1 and b1 < b2) cout << "1";
-	else if (a1 < a2 and b2 < b1) cout << "2";
-	else cout << "?";
-	cout << " , ";
+	int max, min;
 	
-	if (a1 <= a2 and b1 <= b2) {
-		if (a2 <= b1) cout << '[' << a2 << ',' << b1 << ']' << endl;
-		else cout << "[]" << endl; 
-	}
-	else if (a1 <= a2 and b2 <= b1) {
-		if (a2 <= b2) cout << '[' << a2 << ',' << b2 << ']' << endl;
-		else cout << "[]" << endl; 
-	}
-	else if (a2 <= a1 and b1 <= b2) {
-		if (a1 <= b1) cout << '[' << a1 << ',' << b1 << ']' << endl; 
-		else cout << "[]" << endl; 
-	}
-	else if (a2 <= a1 and b2 <= b1) {
-		if (a1 <= b2) cout << '[' << a1 << ',' << b2 << ']' << endl;
-		else cout << "[]" << endl;
-	}
-	else cout << "[]" << endl;
-}
-
-
-//ANOTHER OPTION:
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
-
-    if ((a == c) and (b == d)){
-        cout << "=";
-    }
-
-    else if ((a >= c) and (b <= d)) {
-        cout << "1";
-    }
-
-    else if ((c >= a) and (d <= b)) {
-        cout << "2";
-    }
-
-    else {
-        cout << "?";
-    }
-
-    cout << " , ";
-
-    //mirem punts de tall
-
-    int x, y;
-
-    if (a >= c) x = a; //mirem si a és el valor més alt en el tram baix de l'interval. Si a és gran l'afafem
-        // [] () -> c[ a( d] b) 
-        
-        
-    else x = c; //sino agafem c com a major.
-        // [] () -> a[ c( b] d)
-    
-    
-    if (b <= d) y = b; // mirem on intersecta superiorment (tram alt)
-        // [] () -> a[ c( b] d)
-    else y = d; //sinň agafem el valor de d
-        // [] () -> a[ c( d] b)
-
-    if (x <= y) cout << "[" << x << "," << y << "]" << endl;
-
-
-
-    else cout << "[]" << endl;
+	if (a1 >= a2) max = a1;
+	else max = a2;
+	
+	if (b1 <= b2) min = b1;
+	else min = b2;
+	
+	if (a1 == a2 and b1 == b2) cout << "= , ";
+	else if (a1 == max and b1 == min) cout << "1 , ";
+	else if (a2 == max and b2 == min) cout << "2 , ";
+	else cout << "? , ";
+	
+	if (min < max) cout << "[]" << endl;
+	else cout << '[' << max << ',' << min << ']' << endl;
 }
