03b8a8ec2908d45ff6a01221f76da26f1edd1b0d
julsgasull <juliagasulltw@gmail.com>
2017-02-23 09:19:15 +0100

    Update 16. P19724_campanar.cc

--- P1/16. P19724_campanar.cc
+++ P1/16. P19724_campanar.cc
@@ -4,42 +4,43 @@
 using namespace std;
 
 int main() {
-	int h, m, t;
+	int h, m, t; //hora(hh:mm) i periode temps(t)
+	
 	while (cin >> h >> m >> t) {
-		int c = 0;
-		for (int i = 0; i < t; ++i) {	
+		
+		//Descomposició temps:
+		int hora = h*60 + m; //passar hora a min
+		int dies = t/1440; //1440min == 24h == 1dia
+		int campanes = dies*484; //484 campanades/dia
+		int temps_restant = t%1440;
+		
+		// Bucle per sumar campanades
+		for (int i = 0; i < temps_restant; ++i) {
+			++hora;	//sumar un min
+			if (hora == 1440) hora = 0;	//h == 24:00 --> h = 00:00
 			
-			// Sumant minut a minut
-			if (m == 59) {
-				if (h == 23) {
-					h = 0;
-					m = 0;
-				}
-				else {
-					m = 0;
-					++h;
-				}
-			}
-			else ++m;
+			h = hora/60; //min --> hh:xx
+			m = hora%60; //hh:mm
 			
-			// Veure la hora i sumar campanades
+			//Hora punta (sumar hora que es + 4 quarts)
 			if (m == 1) {
-				if (h == 12) c += 100;
-				else if (h == 0) c += 12;
+				if (h == 12) campanes += 100; //12:00 sonen 100 vegades
+				else if (h == 0) campanes += 12;
 				else {
-					if (13 <= h and h <= 23) c += h - 12;
-					else c += h;
+					if (h > 12) campanes += h - 12;
+					else campanes += h;
 				}
-			c += 4;
+				campanes += 4;
 			}
 			
+			//Quarts
 			else {
-				if (m == 15 + 1) c += 1;
-				else if (m == 30 + 1) c += 2;
-				else if (m == 45 + 1) c += 3;
+				if (m == 15 + 1) campanes += 1;	//xx:16
+				else if (m == 30 + 1) campanes += 2; //xx:31
+				else if (m == 45 + 1) campanes += 3; //xx:46
 			}
 		}
-		
-		cout << c << endl;	
+
+		cout << campanes << endl;
 	}
 }
