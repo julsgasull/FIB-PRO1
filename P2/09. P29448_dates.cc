858658d23e8205c67a1f4297a7c98f79a23427dd
julsgasull <juliagasulltw@gmail.com>
2017-03-02 10:16:40 +0100

    Update 09. P29448_dates.cc

--- P2/09. P29448_dates.cc
+++ P2/09. P29448_dates.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 bool traspas (int n) {
-	if (((n%4) == 0) and (((n%100) != 0) or (n/100)%4 == 0)) return true;
+	if (((n % 4) == 0) and (((n % 100) != 0) or (n / 100) % 4 == 0)) return true;
 	return false;
 }
 
@@ -15,13 +15,13 @@ int main() {
 		bool correcta = false;
 				
 		if (m == 2) {
-			if (traspas(a) and (1<= d and d <= 29)) correcta = true;
-			else if (!traspas(a) and (1<= d and d <= 28)) correcta = true;
+			if (traspas(a) and (1 <= d and d <= 29)) correcta = true;
+			else if (!traspas(a) and (1 <= d and d <= 28)) correcta = true;
 			else correcta = false;
 		}
 
 		else if (m >= 1 and m <= 12) {
-			if ((m%2 != 0 and m <= 7) or (m%2 == 0 and m >= 8)) {
+			if ((m % 2 != 0 and m <= 7) or (m % 2 == 0 and m >= 8)) {
 				if (1 <= d and d <= 31) correcta = true;
 				else correcta = false;
 			}
