79b2d9ca83251623095fae0e3e5c4792b3e03201
Júlia Gasull <juliagasulltw@gmail.com>
2017-05-25 09:36:56 +0200

    Update 06. P52274_Cercles(3).cc

--- P9/06. P52274_Cercles(3).cc
+++ P9/06. P52274_Cercles(3).cc
@@ -1,91 +1,89 @@
-//P52274_Cercles(3).cc
+//06. P52274_cercles3.cc
 
 #include <iostream>
 #include <cmath>
-#include <vector>
 using namespace std;
- 
-struct Punt{
+
+struct Punt {
     double x, y;
 };
- 
-struct Cercle{
+
+struct Cercle {
     Punt centre;
     double radi;
 };
- 
+
+//Llegeix un punt
+void llegeix_punt(Punt& p) {
+	cin >> p.x >> p.y;
+}
+
+//Llegeix un cercle
+void llegeix_cercle(Cercle& c) {
+ cin >> c.centre.x >> c.centre.y >> c.radi;
+}
+
+//funcio que escala un cercle
 void escala(Cercle& c, double esc) {
-   c.radi *= esc;
+    c.radi *= esc;
 }
- 
+
+//funcio que desplaca un cercle segons un punt
+void desplaca(Cercle& c, const Punt& p){
+    c.centre.x += p.x;
+    c.centre.y += p.y;
+}
+
+//funcio que retorna la distancia entre dos punts a(x1, y1) i b (x2, y2)
 double distancia(const Punt& a, const Punt& b) {
-   double m = a.x - b.x;
-   double n = a.y - b.y;
-   m *=m;
-   n *=n;
-   double c = m + n;
-   return sqrt(c);
+    return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
 }
- 
-int max2(int a,int b) {
-  if (a >= b) return a;
-  else return b;
-}
- 
-int min2(int a, int b) {
-   if (a <= b) return a;
-   else return b;
-}
- 
-void desplaca(Punt& p1, const Punt& p2) {
-    p1.x = p1.x + p2.x;
-    p1.y = p1.y + p2.y;
-}
- 
- 
-void llegeix(Cercle& c) {
-   cin >> c.centre.x >> c.centre.y >> c.radi;
-}
- 
-bool es_interior(const Punt& p, const Cercle& c) {
-  int t = int(distancia(c.centre, p));
-  if (t <= c.radi) return true;
-  else return false;
-}
- 
-int relacio(const Cercle& c1, const Cercle& c2) {
-    if (es_interior(c1.centre, c2) and c1.radi < c2.radi) return 1;
-    else if (es_interior(c2.centre, c1) and c2.radi < c1.radi) return 2;
-    else if ((not es_interior(c1.centre, c2)) and (distancia(c1.centre, c2.centre) < (c1.radi+c2.radi))) return 3;
-    else if ((not es_interior(c2.centre, c1)) and (distancia(c1.centre, c2.centre) < (c1.radi+c2.radi))) return 3;
+
+//funcio que indica la relacio de dos cercles
+int relacio(const Cercle& c1, const Cercle& c2){
+    if (distancia(c1.centre, c2.centre) < c1.radi - c2.radi) return 2;
+    if (distancia(c1.centre, c2.centre) < c2.radi - c1.radi) return 1;
+    if (distancia(c1.centre, c2.centre) < c2.radi + c1.radi) return 3;
     else return 0;
 }
- 
-int main() {
-    vector <Cercle> v(2);
-    llegeix(v[0]);
-    llegeix(v[1]);
-    if(relacio(v[0], v[1]) == 1) cout << "el primer cercle es interior al segon" << endl;
-    else if(relacio(v[0], v[1]) == 2) cout << "el segon cercle es interior al primer" << endl;
-    else if(relacio(v[0], v[1]) == 3) cout << "els cercles intersecten" << endl;
-    else if(relacio(v[0], v[1]) == 0) cout << "els cercles no intersecten" << endl;
-    int m;
-    while (cin >> m) {
-    string s;
-    cin >> s;
-    if (s == "escala") {
-      int n;
-      cin >> n;
-      escala(v[m-1], n);    
+
+void imprimir_relacio(const Cercle& c1, const Cercle& c2){
+    if (relacio(c1, c2) == 1) cout << "el primer cercle es interior al segon" << endl;
+    else if (relacio(c1, c2) == 2) cout << "el segon cercle es interior al primer" << endl;
+    else if (relacio(c1, c2) == 3) cout << "els cercles intersecten" << endl;
+    else cout << "els cercles no intersecten" << endl;
+}
+
+int main(){
+    //entren dos cercles c1, c2
+    Cercle c1, c2;
+    llegeix_cercle(c1);
+    llegeix_cercle(c2);
+    
+    imprimir_relacio(c1, c2); //relacio inicial dels cercles
+    
+    int n; //indica a quin cercle cal aplicar el desplacament
+    while (cin >> n){
+	string ordre;	//mou o escala
+	cin >> ordre;
+	
+	//escala
+	if (ordre == "escala") {
+	    double esc;
+	    cin >> esc;
+	    if (n == 1) escala(c1, esc); 	//cercle 1
+	    else escala(c2, esc);		//cercle 2
+	}
+	
+	//mou
+	else {
+	    Punt p;				//punt
+	    llegeix_punt(p);
+	    if (n == 1) desplaca(c1, p);	//cercle 1
+	    else desplaca(c2, p);		//cercle 2
+	}
+	
+	imprimir_relacio(c1, c2); //relacio dels cercles al canviar les posicions
     }
-    else if (s == "mou") {
-      Punt c;
-      cin >> c.x >> c.y;
-      desplaca(v[m-1].centre, c);
-    }
-    if(relacio(v[0], v[1]) == 1) cout << "el primer cercle es interior al segon" << endl;
-    else if(relacio(v[0], v[1]) == 2) cout << "el segon cercle es interior al primer" << endl;
-    else if(relacio(v[0], v[1]) == 3) cout << "els cercles intersecten" << endl;
-    else if(relacio(v[0], v[1]) == 0) cout << "els cercles no intersecten" << endl;
-    }
+    
 }
